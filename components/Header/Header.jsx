4f42ad340358bd292077c701d60ced730cc69680
Paulo Felipe Brito <paulofelipebrito@hotmail.com>
2022-08-01 06:39:31 -0300

    feat: Layout and Header component

---
+++ components/Header/Header.jsx
@@ -0,0 +1,33 @@
+import css from './Header.module.css';
+import Image from 'next/image';
+import Logo from '../../assets/Logo.png';
+import { UilShoppingBag } from '@iconscout/react-unicons' 
+
+const Header = () => {
+  return (
+    <div className={css.header}>
+      
+      {/* Logo */}
+      <div className={css.logo}>
+        <Image src={Logo} alt="" width={50} height={50} />
+      </div>
+
+      {/* Menu */}
+      <ul>
+        <li>Home</li>
+        <li>Menu</li>
+        <li>Contact</li>
+      </ul>
+
+      {/* Right side */}
+      <div className={css.rightSide}>
+        <div className={css.cart}>
+          <UilShoppingBag size={35} color="#2E2E2E"/>
+          <div className={css.badge}>1</div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Header
